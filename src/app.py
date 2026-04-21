e60ee0400a5dc4e092c447fac515c22c3eed28cf
FabianoFR <fabianofariaderesende@gmail.com>
2026-04-21 13:35:59 -0300

    feat: adicionar app.py inicial

---
+++ src/app.py
@@ -0,0 +1,11 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+
+def main():
+    logging.info("Iniciando aplicação...")
+    print("Olá, mundo! Este é meu primeiro projeto em Python no GitHub.")
+    logging.info("Aplicação finalizada com sucesso.")
+
+if __name__ == "__main__":
+    main()
